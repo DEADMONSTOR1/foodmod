1a22bce3d79d602a36e7404324c992ea78e88132
DEADMONSTOR <josh14m@aol.com>
2016-10-19 12:01:29 +0100

    Update init.lua

--- lua/entities/food_base/init.lua
+++ lua/entities/food_base/init.lua
@@ -25,18 +25,11 @@ function ENT:Initialize()
 end
 
 function ENT:Use( activator )
-	if foodmod.mode == 1 then
-		local health = activator:Health()
-		activator:SetHealth( math.Clamp( ( health or 100 ) + foodmod.healthgain, 0, 100 ) )
-	end
-	
-	if foodmod.mode == 2 then
-		local energy = activator:getDarkRPVar("Energy")
-		activator:setSelfDarkRPVar( "Energy", math.Clamp( (energy or 100) + foodmod.energygain, 0, 100 ) )
-	end
+
+	local energy = activator:GetNWInt("Energy", 0)
+	activator:SetNWInt( "Energy", math.Clamp( (energy or 0) + 30, 0, 100 ) )
 	
 	if foodmod.enablesound then
 		activator:EmitSound( self.foodSound, 50, 100 )
 	end
-	self:Remove()
 end
